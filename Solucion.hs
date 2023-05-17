fe8fb60d2c43093ac94817093e1269a23b89ef7e
Martín Dosio <132617486+MartinDosio@users.noreply.github.com>
2023-05-17 13:43:04 -0300

    Mas Prolijidad

--- Solucion.hs
+++ Solucion.hs
@@ -14,7 +14,6 @@ type Relacion = (Usuario, Usuario) -- usuarios que se relacionan
 type Publicacion = (Usuario, String, [Usuario]) -- (usuario que publica, texto publicacion, likes)
 type RedSocial = ([Usuario], [Relacion], [Publicacion])
 
-
 -- Funciones basicas
 
 usuarios :: RedSocial -> [Usuario]
@@ -27,10 +26,10 @@ publicaciones :: RedSocial -> [Publicacion]
 publicaciones (_, _, ps) = ps
 
 idDeUsuario :: Usuario -> Integer
-idDeUsuario (id, _) = id 
+idDeUsuario (id, _) = id
 
 nombreDeUsuario :: Usuario -> String
-nombreDeUsuario (_, nombre) = nombre 
+nombreDeUsuario (_, nombre) = nombre
 
 usuarioDePublicacion :: Publicacion -> Usuario
 usuarioDePublicacion (u, _, _) = u
@@ -43,59 +42,55 @@ likesDePublicacion (_, _, us) = us
 -- Ejercicio 1
 
 -- Recorre la lista de usuarios de la red social y toma los nombres de cada usuario
-
 nombresDeUsuarios :: RedSocial -> [String]
 nombresDeUsuarios red = quitarRepetidos (nombresDeUsuariosAux red)
 
-        where   nombresDeUsuariosAux :: RedSocial -> [String]   -- Sería la función principal, solo que puede contener repetidos.
-                nombresDeUsuariosAux ([], _, _) = []            -- Recorre la lista de usuarios y los va agregando al resultado
-                nombresDeUsuariosAux ((u:us), rs, ps)| longitud us == 0 = [nombreDeUsuario u]
-                                                | pertenece u us   = nombresDeUsuarios (us, rs, ps)
-                                                | otherwise        = nombreDeUsuario u : nombresDeUsuarios (us, rs, ps)
+              -- Sería la función principal, solo que puede contener repetidos. Recorre la lista de usuarios y los va agregando al resultado
+        where nombresDeUsuariosAux :: RedSocial -> [String]   
+              nombresDeUsuariosAux ([], _, _) = []
+              nombresDeUsuariosAux (u:us, rs, ps) | longitud us == 0 = [nombreDeUsuario u]
+                                                  | pertenece u us = nombresDeUsuarios (us, rs, ps)
+                                                  | otherwise = nombreDeUsuario u : nombresDeUsuarios (us, rs, ps)
 
-                                  
 -- Ejercicio 2
 
 -- Dado un usuario, devuelve una lista con sus amigos
-
 amigosDe :: RedSocial -> Usuario -> [Usuario]
-amigosDe (us,rs,ps) user = quitarRepetidos(amigosDeAux rs user)
+amigosDe (us,rs,ps) user = quitarRepetidos (amigosDeAux rs user)
 
-        where   amigosDeAux :: [Relacion] -> Usuario -> [Usuario]
-                amigosDeAux [] _ = []
-                amigosDeAux (r:rel) user | user1 == user = user2 : amigosDeAux rel user
-                                        | user2 == user = user1 : amigosDeAux rel user
-                                        | otherwise = amigosDeAux rel user
-                                        where   user1 = fst r
-                                                user2 = snd r  
+              --
+        where amigosDeAux :: [Relacion] -> Usuario -> [Usuario]
+              amigosDeAux [] _ = []
+              amigosDeAux (r:rel) user | user1 == user = user2 : amigosDeAux rel user
+                                       | user2 == user = user1 : amigosDeAux rel user
+                                       | otherwise = amigosDeAux rel user
+                                        where user1 = fst r
+                                              user2 = snd r
 
 -- Ejercicio 3
 
 -- Dado un usuario, devuelve la cantidad de amigos que tiene
-
 cantidadDeAmigos :: RedSocial -> Usuario -> Int
 cantidadDeAmigos rs us = fromIntegral (longitud (amigosDe rs us))
 
 -- Ejercicio 4
- 
--- Dada una red válidoa con al menos un usuario, devuelve el usuario con más amigos
 
+-- Dada una red válidoa con al menos un usuario, devuelve el usuario con más amigos
 usuarioConMasAmigos :: RedSocial -> Usuario     -- Se fija entre los primeros 2 usuarios cuál tiene más amigos, y repite con el resto de los usuarios
 usuarioConMasAmigos ([user], _, _) = user       -- y el que tenga más de estos. Cuando solo quede un usuario, ese será el que tenga más amigos
-usuarioConMasAmigos ((user1:user2:us), rs, ps)
-        | longitud us == 0 && cantidadDeAmigos red user1 >= cantidadDeAmigos red user2 = user1
-        | longitud us == 0 && cantidadDeAmigos red user2 > cantidadDeAmigos red user1 = user2
-        | cantidadDeAmigos red user1 >= cantidadDeAmigos red user2 = usuarioConMasAmigos (user1:us,rs,ps) 
-        | otherwise = usuarioConMasAmigos (user2:us,rs,ps)
-        where red = ((user1:user2:us), rs, ps)
-              
+usuarioConMasAmigos (user1:user2:us, rs, ps) | longitud us == 0 && cantidadDeAmigos red user1 >= cantidadDeAmigos red user2 = user1
+                                             | longitud us == 0 && cantidadDeAmigos red user2 > cantidadDeAmigos red user1 = user2
+                                             | cantidadDeAmigos red user1 >= cantidadDeAmigos red user2 = usuarioConMasAmigos (user1:us,rs,ps)
+                                             | otherwise = usuarioConMasAmigos (user2:us,rs,ps)
+                                             where red = (user1:user2:us, rs, ps)
+
 -- Ejercicio 5
-              
+
 -- describir qué hace la función: .....
 estaRobertoCarlos :: RedSocial -> Bool
 estaRobertoCarlos ([], _, _) = False
 estaRobertoCarlos red = cantidadDeAmigos red (usuarioConMasAmigos red) > 10
-                                   
+
 -- Ejercicio 6
 
 -- describir qué hace la función: .....
@@ -103,20 +98,21 @@ publicacionesDe :: RedSocial -> Usuario -> [Publicacion]
 publicacionesDe (_, _, []) _ = []
 publicacionesDe red user = quitarRepetidos (publicacionesDeAux red user)
 
-        where   publicacionesDeAux :: RedSocial -> Usuario -> [Publicacion]
-                publicacionesDeAux (us, rs, (p:ps)) user | longitud ps == 0 && usuarioDePublicacion p == user = [p]
-                                                | longitud ps == 0 && not (usuarioDePublicacion p == user) = []
-                                                | usuarioDePublicacion p == user = p : publicacionesDeAux (us, rs, ps) user
-                                                | otherwise = publicacionesDeAux (us, rs, ps) user
+              --
+        where publicacionesDeAux :: RedSocial -> Usuario -> [Publicacion]
+              publicacionesDeAux (us, rs, p:ps) user | longitud ps == 0 && usuarioDePublicacion p == user = [p]
+                                                     | longitud ps == 0 && (usuarioDePublicacion p /= user) = []
+                                                     | usuarioDePublicacion p == user = p : publicacionesDeAux (us, rs, ps) user
+                                                     | otherwise = publicacionesDeAux (us, rs, ps) user
 
 -- Ejercicio 7
 
 -- describir qué hace la función: .....
 publicacionesQueLeGustanA :: RedSocial -> Usuario -> [Publicacion]
-publicacionesQueLeGustanA (us, rs, (p:ps)) user | longitud ps == 0 && not (pertenece user (likesDePublicacion p)) = []  
-                                                | longitud ps == 0 && pertenece user (likesDePublicacion p) = [p]
-                                                | pertenece user (likesDePublicacion p) = p : publicacionesQueLeGustanA (us, rs, ps) user
-                                                | otherwise                             = publicacionesQueLeGustanA (us, rs, ps) user                    
+publicacionesQueLeGustanA (us, rs, p:ps) user | longitud ps == 0 && not (pertenece user (likesDePublicacion p)) = []
+                                              | longitud ps == 0 && pertenece user (likesDePublicacion p) = [p]
+                                              | pertenece user (likesDePublicacion p) = p : publicacionesQueLeGustanA (us, rs, ps) user
+                                              | otherwise = publicacionesQueLeGustanA (us, rs, ps) user
 
 -- Ejercicio 8
 
@@ -125,7 +121,6 @@ lesGustanLasMismasPublicaciones :: RedSocial -> Usuario -> Usuario -> Bool
 lesGustanLasMismasPublicaciones red u1 u2 = sonPermutacion (publicacionesQueLeGustanA red u1) (publicacionesQueLeGustanA red u2)
 -- Como pueden estar en orden diferente un == no alcanza, y por eso hay que verificar que sean permutaciones entre sí
 
-
 -- Ejercicio 9
 
 -- Dada una red social y un usuario verifica si existe un usuario que le haya dado like a todas las publicaciones de otro usuario, que tienen que ser al menos una
@@ -133,34 +128,32 @@ tieneUnSeguidorFiel :: RedSocial -> Usuario -> Bool
 tieneUnSeguidorFiel r u | longitud (publicacionesDe r u) == 0 = False
                         | otherwise = tieneUnSeguidorFielAux2 (tail (publicacionesDe r u)) (likesDePublicacion (head (publicacionesDe r u)))
 
-                -- Verifica si alguno de los usuarios de la lista de likes de la primera publicación es un seguidor fiel
-        where   tieneUnSeguidorFielAux2 :: [Publicacion] -> [Usuario] -> Bool
-                tieneUnSeguidorFielAux2 _ [] = False
-                tieneUnSeguidorFielAux2 [] us = longitud us >= 1
-                tieneUnSeguidorFielAux2 p (u:us) | longitud us == 0 = tieneUnSeguidorFielAux p u
-                                                | otherwise = tieneUnSeguidorFielAux p u || tieneUnSeguidorFielAux2 p us
+              -- Verifica si alguno de los usuarios de la lista de likes de la primera publicación es un seguidor fiel
+        where tieneUnSeguidorFielAux2 :: [Publicacion] -> [Usuario] -> Bool
+              tieneUnSeguidorFielAux2 _ [] = False
+              tieneUnSeguidorFielAux2 [] us = longitud us >= 1
+              tieneUnSeguidorFielAux2 p (u:us) | longitud us == 0 = tieneUnSeguidorFielAux p u
+                                                 | otherwise = tieneUnSeguidorFielAux p u || tieneUnSeguidorFielAux2 p us
 
-                -- Verifica si un usuario ha dado like a todas las publicaciones de 'u'
-                tieneUnSeguidorFielAux :: [Publicacion] -> Usuario -> Bool
-                tieneUnSeguidorFielAux (p:ps) u | longitud ps == 0 = pertenece u (likesDePublicacion p)
+              -- Verifica si un usuario ha dado like a todas las publicaciones de 'u'
+              tieneUnSeguidorFielAux :: [Publicacion] -> Usuario -> Bool
+              tieneUnSeguidorFielAux (p:ps) u | longitud ps == 0 = pertenece u (likesDePublicacion p)
                                                 | pertenece u (likesDePublicacion p) = tieneUnSeguidorFielAux ps u
                                                 | otherwise = False
 
 -- Ejercicio 10
 
 -- describir qué hace la función: .....
-          
 existeSecuenciaDeAmigos :: RedSocial -> Usuario -> Usuario -> Bool
 existeSecuenciaDeAmigos red usuario1 usuario2 = existeSecuenciaDeAmigosEntreSusAmigos red usuario1 usuario2 []
 
-    where   existeSecuenciaDeAmigosEntreSusAmigos :: RedSocial -> Usuario -> Usuario -> [Usuario] -> Bool
-            existeSecuenciaDeAmigosEntreSusAmigos red user1 user2 repetidos
-                | pertenece user1 repetidos = False
-                | pertenece (user1, user2) (relaciones red) || pertenece (user2, user1) (relaciones red) = True
-                | otherwise = anadirAmigoARepetidosYRepetir (amigosDe red user1)
-
-                where   anadirAmigoARepetidosYRepetir :: [Usuario] -> Bool
-                        anadirAmigoARepetidosYRepetir [] = False
-                        anadirAmigoARepetidosYRepetir (u:us)
-                            | u == user1 = False
-                            | otherwise = existeSecuenciaDeAmigosEntreSusAmigos red u user2 (user1:repetidos) || anadirAmigoARepetidosYRepetir us
+               --
+        where  existeSecuenciaDeAmigosEntreSusAmigos :: RedSocial -> Usuario -> Usuario -> [Usuario] -> Bool
+               existeSecuenciaDeAmigosEntreSusAmigos red user1 user2 repetidos | pertenece user1 repetidos = False
+                                                                               | pertenece (user1, user2) (relaciones red) || pertenece (user2, user1) (relaciones red) = True
+                                                                               | otherwise = anadirAmigoARepetidosYRepetir (amigosDe red user1)
+                      --
+                where anadirAmigoARepetidosYRepetir :: [Usuario] -> Bool
+                      anadirAmigoARepetidosYRepetir [] = False
+                      anadirAmigoARepetidosYRepetir (u:us) | u == user1 = False
+                                                           | otherwise = existeSecuenciaDeAmigosEntreSusAmigos red u user2 (user1:repetidos) || anadirAmigoARepetidosYRepetir us
