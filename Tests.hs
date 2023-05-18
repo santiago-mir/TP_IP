b842a111a03c45ba79cf35ad1f4d42b14bff71b4
LucasGetsCode <lucas.pujia@gmail.com>
2023-05-18 15:44:49 -0300

    Info sobre tests y organización

--- Tests.hs
+++ Tests.hs
@@ -13,13 +13,13 @@ usuario4 = (4, "Mariela")   -- A, C
 usuario5 = (5, "Natalia")   -- B, C 
 usuario6 = (6, "Roberto")
 usuario7 = (7, "María")
-usuario8 = (8, "Luis")
+usuario8 = (8, "Natalia")
 usuario9 = (9, "Don José")
 usuario10 = (10, "Lucía")
 usuario11 = (11, "Alex")
 usuario12 = (12, "Alex")
 
-
+-- relaciones básicas
 relacion1_2 = (usuario1, usuario2)  -- A, B, C
 relacion1_3 = (usuario1, usuario3) 
 relacion1_4 = (usuario4, usuario1)  -- A
@@ -56,6 +56,7 @@ relacion9_10 = (usuario9, usuario10)
 relacion10_11 = (usuario10, usuario11)
 relacion11_12 = (usuario11, usuario12)
 
+-- publicaciones básicas
 publicacion1_1 = (usuario1, "Este es mi primer post", [usuario2, usuario4]) -- A, C
 publicacion1_2 = (usuario1, "Este es mi segundo post", [usuario4])          -- A
 publicacion1_3 = (usuario1, "Este es mi tercer post", [usuario2, usuario5]) -- B
@@ -83,19 +84,42 @@ publicacion1_9 =  (usuario1, "Sigo solito", [])  -- Tests ej7
 usuariosA = [usuario1, usuario2, usuario3, usuario4]
 relacionesA = [relacion1_2, relacion1_4, relacion2_3, relacion2_4, relacion3_4]
 publicacionesA = [publicacion1_1, publicacion1_2, publicacion2_1, publicacion2_2, publicacion3_1, publicacion3_2, publicacion4_1, publicacion4_2, publicacion2_1i]
-redA = (usuariosA, relacionesA, publicacionesA) -- RedA
+redA = (usuariosA, relacionesA, publicacionesA) -- RedA, básica
 
 usuariosB = [usuario1, usuario2, usuario3, usuario5]
 relacionesB = [relacion1_2, relacion2_3]
 publicacionesB = [publicacion1_3, publicacion1_4, publicacion1_5, publicacion3_1, publicacion3_2, publicacion3_3]
-redB = (usuariosB, relacionesB, publicacionesB) -- RedB
+redB = (usuariosB, relacionesB, publicacionesB) -- RedB, básica con nombre de usuario repetido
 
 usuariosC = [usuario1, usuario2, usuario3, usuario4, usuario5]
 relacionesC = [relacion1_2, relacion2_3, relacion3_4]
 publicacionesC = [publicacion1_1, publicacion1_4]
-redC = (usuariosC, relacionesC, publicacionesC) -- RedC
+redC = (usuariosC, relacionesC, publicacionesC) -- RedC, básica con pocas publicaciones
+
+usuariosMuchos = [usuario1,usuario2,usuario3,usuario4,usuario5,usuario6,usuario7,usuario8,usuario9,usuario10,usuario11,usuario12]
+
+relacionesEnCadena = [relacion1_2, relacion2_3, relacion3_4, relacion4_5, relacion3_5, relacion2_5,
+    relacion1_3,relacion6_8, relacion9_10, relacion7_8, relacion6_7, relacion11_12, relacion10_11]
+
+relacionesMuchas = [relacion1_2,relacion1_3,relacion1_4,relacion2_3,relacion2_4,relacion3_4,relacion4_5,
+    relacion1_5,relacion1_6,relacion1_7,relacion1_8,relacion1_9, relacion1_10,relacion1_11,relacion1_12]
+
+relacionesMuchasMas = relacionesMuchas ++ [relacion1_2,relacion2_3,relacion2_4,relacion2_5,relacion2_6,
+    relacion2_7,relacion2_8,relacion2_9, relacion2_10,relacion2_11,relacion2_12]
+
+redVacia = ([], [], [])
+redSinRelaciones = (usuariosA, [], [])
+red2 = ([usuario1], [], []) -- red2, solo con un único usuario
+red3 = (usuariosA, [], [])  -- red3, solo con un grupo de usuarios
+red4 = (usuariosB, [], [])  -- red4, solo con un grupo de usuarios, diferentes a los de la red3
+red5 = (usuariosMuchos, [relacion1_2, relacion2_3, relacion3_4, relacion4_5], [])   -- red5, muchos usuarios con algunas relaciones
+red6 = (usuariosMuchos, relacionesEnCadena, []) -- red6, muchos usuarios y muchas relaciones
+redRobertoCarlos = (usuariosMuchos, relacionesMuchas, publicacionesA) -- redRobertoCarlos
+redRobertoCarlos2 = (usuariosMuchos, relacionesMuchasMas, []) -- redRobertoCarlos 2
 
 --------------------------------------------------------------------------------------
+
+-- Tests:
 
 expectAny actual expected = elem actual expected ~? ("expected any of: " ++ show expected ++ "\n but got: " ++ show actual)
 esPermutacion actual expected = sonPermutacion actual expected ~? ("expected permutacion of: " ++ show expected ++ "\n but got: " ++ show actual)
@@ -115,30 +139,16 @@ todosLosTests = test [testSuiteEj1, testSuiteEj2, testSuiteEj3, testSuiteEj4, te
 
 main = runTestTT todosLosTests
 
-usuariosMuchos = [usuario1,usuario2,usuario3,usuario4,usuario5,usuario6,usuario7,usuario8,usuario9,usuario10,usuario11,usuario12]
-relacionesMuchas = [relacion1_2,relacion1_3,relacion1_4,relacion2_3,relacion2_4,relacion3_4,relacion4_5,
-    relacion1_5,relacion1_6,relacion1_7,relacion1_8,relacion1_9, relacion1_10,relacion1_11,relacion1_12]
-relacionesMuchasMas = relacionesMuchas ++ [relacion1_2,relacion2_3,relacion2_4,relacion2_5,relacion2_6,
-    relacion2_7,relacion2_8,relacion2_9, relacion2_10,relacion2_11,relacion2_12]
-
-redVacia = ([], [], [])
-redSinRelaciones = (usuariosA, [], [])
-red2 = ([usuario1], [], []) -- red2, solo con un único usuario
-red3 = (usuariosA, [], [])  -- red3, solo con un grupo de usuarios
-red4 = (usuariosB, [], [])  -- red4, solo con un grupo de usuarios, diferentes a los de la red3
-red5 = (usuariosMuchos, [relacion1_2, relacion2_3, relacion3_4, relacion4_5], [])   -- red5, muchos usuarios con algunas relaciones
-red6 = (usuariosMuchos, [relacion1_2, relacion2_3, relacion3_4, relacion4_5, relacion3_5, relacion2_5, relacion1_3,
-    relacion6_8, relacion9_10, relacion7_8, relacion6_7, relacion11_12, relacion10_11], []) -- red6, muchos usuarios y muchas relaciones
-redRobertoCarlos = (usuariosMuchos, relacionesMuchas, publicacionesA) -- redRobertoCarlos
-redRobertoCarlos2 = (usuariosMuchos, relacionesMuchasMas, []) -- redRobertoCarlos 2
+---- TestSuites:
 
 -- nombresDeUsuarios
 testSuiteEj1 = test [
     -- las relaciones y publicaciones no tienen relevancia en este ejercicio, no se toman en cuenta
     "Caso 1: RedSocial sin usuarios" ~: (nombresDeUsuarios redVacia) ~?= [],
     "Caso 2: RedSocial con un solo usuario" ~: (nombresDeUsuarios red2) ~?= ["Juan"],
-    "Caso 3: RedSocial con más de un usuario" ~: (nombresDeUsuarios red3) ~?= ["Juan", "Natalia", "Pedro", "Mariela"],
-    "Caso 4: RedSocial con usuarios del mismo nombre" ~: (nombresDeUsuarios red4) ~?= ["Juan", "Natalia", "Pedro"]
+    "Caso 3: RedSocial con más de un usuario" ~: esPermutacion (nombresDeUsuarios red3) ["Juan", "Natalia", "Pedro", "Mariela"],
+    "Caso 4: RedSocial con usuarios del mismo nombre" ~: esPermutacion (nombresDeUsuarios red4) ["Juan", "Natalia", "Pedro"],
+    "Caso 4: RedSocial con usuarios, varios casos del mismo nombre" ~: esPermutacion (nombresDeUsuarios red5) ["Juan", "Natalia", "Pedro","Mariela","Alex","Lucía","Roberto","María","Don José"]
     ]
 
 -- amigosDe
@@ -149,7 +159,7 @@ testSuiteEj2 = test [
     "Caso 2: RedSocial con más de un usuario, sin relaciones" ~: (amigosDe red3 usuario1) ~?= [],
     "Caso 3: RedSocial con más de un usuario y relaciones, usuario sin amigos" ~: (amigosDe redC usuario5) ~?= [],
     "Caso 3: RedSocial con más de un usuario y relaciones, usuario con un amigo" ~: (amigosDe redC usuario1) ~?= [(2, "Natalia")],
-    "Caso 4: RedSocial con más de un usuario y relaciones, usuario con más de un amigo" ~: (amigosDe redA usuario2) ~?= [(1, "Juan"), (3, "Pedro"), (4, "Mariela")]
+    "Caso 4: RedSocial con más de un usuario y relaciones, usuario con más de un amigo" ~: esPermutacion (amigosDe redA usuario2) [(1, "Juan"), (3, "Pedro"), (4, "Mariela")]
     ]
 
 -- cantidadDeAmigos
@@ -175,7 +185,7 @@ testSuiteEj4 = test [
 testSuiteEj5 = test [
     -- Las publicaciones no tienen relevancia en este ejercicio, no se tienen en cuenta
     "Caso 1: RedSocial sin usuarios ni relaciones" ~: (estaRobertoCarlos redVacia) ~?= False,
-    "Caso 2: RedSocial con un unico usuario, sin relaciones" ~: (estaRobertoCarlos red2) ~?= False,
+    "Caso 2: RedSocial con un único usuario, sin relaciones" ~: (estaRobertoCarlos red2) ~?= False,
     "Caso 3: RedSocial con más de un usuario, sin relaciones" ~: (estaRobertoCarlos red3) ~?= False,
     "Caso 4: RedSocial con más de un usuario, pero ninguno con más de 10 amigos" ~: (estaRobertoCarlos redA) ~?= False,
     "Caso 5: RedSocial con más de un usuario, alguno con más de 10 amigos" ~: (estaRobertoCarlos redRobertoCarlos) ~?= True,
@@ -186,11 +196,11 @@ testSuiteEj5 = test [
 testSuiteEj6 = test [
     -- El caso de red social vacia no aplica, la especificacion pide que la red social tenga por lo menos un usuario
     -- Las relaciones no tienen relevancia en este ejercicio, no se tienen en cuenta
-    "Caso 1: RedSocial con un unico usuario, sin publicaciones" ~: (publicacionesDe red2 usuario1) ~?= [],
-    "Caso 2: RedSocial con más de un usuario, sin publicaciones" ~: (publicacionesDe red3 usuario1) ~?= [],
-    "Caso 3: RedSocial con más de un usuario y publicaciones, usuario5 sin publicaciones" ~: (publicacionesDe redC usuario5) ~?= [],
-    "Caso 4: RedSocial con más de un usuario y publicaciones, usuario1 con publicaciones" ~: esPermutacion (publicacionesDe redA usuario1) [publicacion1_1, publicacion1_2]  
-    --  Que hace este test? " publicacionesDe 1" ~: esPermutacion (publicacionesDe redA usuario2) [publicacion2_1, publicacion2_2],
+    "Caso 1: RedSocial con un único usuario, sin publicaciones" ~: (publicacionesDe red2 usuario1) ~?= [],
+    "Caso 2: RedSocial con un único usuario, con publicaciones" ~: esPermutacion (publicacionesDe ([usuario1],[],[publicacion1_6,publicacion1_7]) usuario1) [publicacion1_7,publicacion1_6],
+    "Caso 3: RedSocial con más de un usuario, sin publicaciones" ~: (publicacionesDe red3 usuario1) ~?= [],
+    "Caso 4: RedSocial con más de un usuario y publicaciones, usuario5 sin publicaciones" ~: (publicacionesDe redC usuario5) ~?= [],
+    "Caso 5: RedSocial con más de un usuario y publicaciones, usuario1 con publicaciones" ~: esPermutacion (publicacionesDe redA usuario1) [publicacion1_1, publicacion1_2]
     ]
 
 -- publicacionesQueLeGustanA
